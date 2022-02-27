b140e4a7bdb36dc673b57ae1dde8186602c33a9b
Samuel-de-Oliveira <samwolfg13@gmail.com>
2022-02-27 16:30:38 -0300

    edit

--- bengala_upgraded.ino
+++ bengala_upgraded.ino
@@ -1,6 +1,14 @@
+/*
+ * --*--* Bengala_upgraded *--*--
+ * 
+ * Author: Samuel de Oliveira (Github: Samuel-de-Oliveira)
+ * Contribuitors: None
+ *
+ */
+
 #include <HCSR04.h>
 
-// Here is the Pins scheme, you can change it if you want :)
+// Here is the pins scheme, you can change it if you want :)
 const byte triggerPin = 3;
 const byte echoPin    = 4;
 const byte buzzerPin  = 9;
@@ -8,22 +16,22 @@ const byte buzzerPin  = 9;
 UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 
 void setup() {
+	pinMode(buzzerPin, OUTPUT);
 	Serial.begin(9600);
 }
 
 void loop () {
 	    float distance = distanceSensor.measureDistanceCm();
-		
 
-	    if (12 < distance and distance <= 30) {
-		    tone(buzzerPin, 600, 250);
+	    if (25 < distance and distance <= 50) {
+
+		    tone(buzzerPin, 800, 300);
 		    delay(250);
 		    noTone(buzzerPin);
 		    delay(500);
+	    } else if (distance <= 25) {
 
-	    } else if (distance <= 12) {
-
-		    tone(buzzerPin, 600, 250);
+		    tone(buzzerPin, 800, 300);
 		    delay(250);
 		    noTone(buzzerPin);
 		    delay(250);
